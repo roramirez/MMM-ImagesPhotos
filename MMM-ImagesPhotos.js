c9d7facc8785af8fda4db61042900f5ea7ed6b03
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-12-19 22:29:59 +0100

    Review - Add keywords to package.json - Replace grunt by node scripts - Rework linter and fix linter issues - Remove unused request and url packages - Replace XMLHttpRequest by fetch - Add changelog entry

--- MMM-ImagesPhotos.js
+++ MMM-ImagesPhotos.js
@@ -1,355 +1,359 @@
-/* global Module */
+/* global Log MM Module */
 
-/* Magic Mirror
+/*
+ * MagicMirror²
  * Module: MMM-ImagesPhotos
  *
  * By Rodrigo Ramírez Norambuena https://rodrigoramirez.com
  * MIT Licensed.
  */
-const ourModuleName="MMM-ImagesPhotos"
+const ourModuleName = "MMM-ImagesPhotos";
 
-Module.register(ourModuleName,{
-	defaults: {
-		opacity: 0.9,
-		animationSpeed: 500,
-		updateInterval: 5000,
-		getInterval: 60000,
-		maxWidth: "100%",
-		maxHeight: "100%",
-		retryDelay: 2500,
-		path: "",
-		fill: false,
-		blur: 8,
-		sequential:false
-	},
+Module.register(ourModuleName, {
+  defaults: {
+    opacity: 0.9,
+    animationSpeed: 500,
+    updateInterval: 5000,
+    getInterval: 60000,
+    maxWidth: "100%",
+    maxHeight: "100%",
+    retryDelay: 2500,
+    path: "",
+    fill: false,
+    blur: 8,
+    sequential: false
+  },
 
-	wrapper: null,
-	suspended: false,
-	timer:null,
-	fullscreen: false,
+  wrapper: null,
+  suspended: false,
+  timer: null,
+  fullscreen: false,
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
+  requiresVersion: "2.24.0", // Required version of MagicMirror
 
-	start: function() {
-		var self = this;
-		this.photos = [];
-		this.loaded = false;
-		this.lastPhotoIndex = -1;
-		this.config['id']=this.identifier
-		this.sendSocketNotification("CONFIG", this.config);
-	},
-	getStyles: function() {
-		return ["MMM-ImagesPhotos.css"];
-	},
+  start() {
+    this.photos = [];
+    this.loaded = false;
+    this.lastPhotoIndex = -1;
+    this.config.id = this.identifier;
+    this.sendSocketNotification("CONFIG", this.config);
+  },
+  getStyles() {
+    return ["MMM-ImagesPhotos.css"];
+  },
 
-	/*
-   * getPhotos
+  /*
    * Requests new data from api url helper
-   *
    */
-	getPhotos: function() {
-		var urlApHelper = "/MMM-ImagesPhotos/photos/"+this.identifier;
-		var self = this;
-		var retry = true;
+  async getPhotos() {
+    const urlApHelper = `/MMM-ImagesPhotos/photos/${this.identifier}`;
+    const self = this;
+    let retry = true;
 
-		var photosRequest = new XMLHttpRequest();
-		photosRequest.open("GET", urlApHelper, true);
-		photosRequest.onreadystatechange = function() {
-			if (this.readyState === 4) {
-				if (this.status === 200) {
-					self.processPhotos(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					Log.error(self.name, "Could not load photos.");
-				}
+    try {
+      const photosResponse = await fetch(urlApHelper);
 
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		photosRequest.send();
-	},
-	notificationReceived(notification,payload,sender){
-		// hook to turn off messages about notiofications, clock once a second
-		if(notification=="ALL_MODULES_STARTED"){
-			const ourInstances=MM.getModules().withClass(ourModuleName)
-			ourInstances.forEach(m=>{
-				if(m.data.position.toLowerCase().startsWith("fullscreen")){
-					this.fullscreen= true;
-				}
-			})
+      if (photosResponse.ok) {
+        const photosData = await photosResponse.json();
+        self.processPhotos(photosData);
+      } else if (photosResponse.status === 401) {
+        self.updateDom(self.config.animationSpeed);
+        Log.error(self.name, photosResponse.status);
+        retry = false;
+      } else {
+        Log.error(self.name, "Could not load photos.");
+      }
 
-		}
-	},
-	startTimer: function(){
-		let self = this;
-		self.timer=setTimeout(() => {
-			  // clear timer value for resume
-			self.timer=null;
-			if(self.suspended==false){
-				self.updateDom(self.config.animationSpeed);
-			}
-		}, this.config.updateInterval);
-	},
+      if (!photosResponse.ok) {
+        if (retry) {
+          self.scheduleUpdate(self.loaded ? -1 : self.config.retryDelay);
+        }
+      }
+    } catch (error) {
+      Log.error(self.name, error);
+    }
+  },
+  notificationReceived(notification, payload, sender) {
+    // Hook to turn off messages about notiofications, clock once a second
+    if (notification === "ALL_MODULES_STARTED") {
+      const ourInstances = MM.getModules().withClass(ourModuleName);
+      ourInstances.forEach((m) => {
+        if (m.data.position.toLowerCase().startsWith("fullscreen")) {
+          this.fullscreen = true;
+        }
+      });
+    }
+  },
+  startTimer() {
+    const self = this;
+    self.timer = setTimeout(() => {
+      // Clear timer value for resume
+      self.timer = null;
+      if (self.suspended === false) {
+        self.updateDom(self.config.animationSpeed);
+      }
+    }, this.config.updateInterval);
+  },
 
-	socketNotificationReceived(notification, payload, source){
-		if(notification == "READY" && payload===this.identifier) {
-			let self = this;
-			// Schedule update timer.
-			this.getPhotos();
-			//this.startTimer();
-		}
-	},
-	/* scheduleUpdate()
+  socketNotificationReceived(notification, payload, source) {
+    if (notification === "READY" && payload === this.identifier) {
+      // Schedule update timer.
+      this.getPhotos();
+    }
+  },
+
+  /*
    * Schedule next update.
    *
    * argument delay number - Milliseconds before next update.
    *  If empty, this.config.updateInterval is used.
    */
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.getInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getPhotos();
-		}, nextLoad);
-	},
+  scheduleUpdate(delay) {
+    let nextLoad = this.config.getInterval;
+    if (typeof delay !== "undefined" && delay >= 0) {
+      nextLoad = delay;
+    }
 
-	/* randomIndex(photos)
+    const self = this;
+    setTimeout(() => {
+      self.getPhotos();
+    }, nextLoad);
+  },
+
+  /*
    * Generate a random index for a list of photos.
    *
    * argument photos Array<String> - Array with photos.
    *
    * return Number - Random index.
    */
-	randomIndex: function(photos) {
-		if (photos.length === 1) {
-			return 0;
-		}
+  randomIndex(photos) {
+    if (photos.length === 1) {
+      return 0;
+    }
 
-		var generate = function() {
-			return Math.floor(Math.random() * photos.length);
-		};
+    const generate = () => Math.floor(Math.random() * photos.length);
 
-		var photoIndex = (this.lastPhotoIndex==photos.length-1?0:this.lastPhotoIndex+1);
-		if(!this.config.sequential)
-			photoIndex =generate();
-		this.lastPhotoIndex = photoIndex;
+    let photoIndex =
+      this.lastPhotoIndex === photos.length - 1 ? 0 : this.lastPhotoIndex + 1;
+    if (!this.config.sequential) {
+      photoIndex = generate();
+    }
+    this.lastPhotoIndex = photoIndex;
 
-		return photoIndex;
-	},
+    return photoIndex;
+  },
 
-	/* complimentArray()
+  /*
    * Retrieve a random photos.
    *
    * return photo Object - A photo.
    */
-	randomPhoto: function() {
-		var photos = this.photos;
-		var index = this.randomIndex(photos);
+  randomPhoto() {
+    const { photos } = this;
+    const index = this.randomIndex(photos);
 
-		return photos[index];
-	},
-
-	ScaleImage: function(srcwidth, srcheight, targetwidth, targetheight, fLetterBox) {
-
-		var result = { width: 0, height: 0, fScaleToTargetWidth: true };
-
-		if ((srcwidth <= 0) || (srcheight <= 0) || (targetwidth <= 0) || (targetheight <= 0)) {
-			return result;
-		}
-
-		// scale to the target width
-		var scaleX1 = targetwidth;
-		var scaleY1 = (srcheight * targetwidth) / srcwidth;
-
-		// scale to the target height
-		var scaleX2 = (srcwidth * targetheight) / srcheight;
-		var scaleY2 = targetheight;
-
-		// now figure out which one we should use
-		var fScaleOnWidth = (scaleX2 > targetwidth);
-		if (fScaleOnWidth) {
-			fScaleOnWidth = fLetterBox;
-		}
-		else {
-			fScaleOnWidth = !fLetterBox;
-		}
-
-		if (fScaleOnWidth) {
-			result.width = Math.floor(scaleX1);
-			result.height = Math.floor(scaleY1);
-			result.fScaleToTargetWidth = true;
-		}
-		else {
-			result.width = Math.floor(scaleX2);
-			result.height = Math.floor(scaleY2);
-			result.fScaleToTargetWidth = false;
-		}
-		result.targetleft = Math.floor((targetwidth - result.width) / 2);
-		result.targettop = Math.floor((targetheight - result.height) / 2);
-
-		return result;
-	},
-
-	suspend: function(){
-		this.suspended=true;
-		if(this.timer!=null){
-			clearTimeout(this.timer);
-			this.timer=null;
-		}
-	},
-	resume: function(){
-		this.suspended=false;
-		if(this.timer==null)
-		{this.startTimer();}
-	},
-
-  getDom(){
-  	if(this.fullscreen)
-  		return this.getDomFS()
-  	else
-  		return this.getDomnotFS()
+    return photos[index];
   },
 
-  getDomnotFS(){
-		var self = this;
-		var wrapper = document.createElement("div");
-		var photoImage = this.randomPhoto();
+  scaleImage(srcwidth, srcheight, targetwidth, targetheight, fLetterBox) {
+    const result = { width: 0, height: 0, fScaleToTargetWidth: true };
 
-		if (photoImage) {
-			var img = document.createElement("img");
-			img.src = photoImage.url;
-			img.id = "mmm-images-photos";
-			img.style.maxWidth = this.config.maxWidth;
-			img.style.maxHeight = this.config.maxHeight;
-			img.style.opacity = self.config.opacity;
-			img.className = "bgimage"
-			wrapper.appendChild(img);
-			self.startTimer();
-		}
-		return wrapper;
+    if (
+      srcwidth <= 0 ||
+      srcheight <= 0 ||
+      targetwidth <= 0 ||
+      targetheight <= 0
+    ) {
+      return result;
+    }
+
+    // Scale to the target width
+    const scaleX1 = targetwidth;
+    const scaleY1 = (srcheight * targetwidth) / srcwidth;
+
+    // Scale to the target height
+    const scaleX2 = (srcwidth * targetheight) / srcheight;
+    const scaleY2 = targetheight;
+
+    // Now figure out which one we should use
+    let fScaleOnWidth = scaleX2 > targetwidth;
+    if (fScaleOnWidth) {
+      fScaleOnWidth = fLetterBox;
+    } else {
+      fScaleOnWidth = !fLetterBox;
+    }
+
+    if (fScaleOnWidth) {
+      result.width = Math.floor(scaleX1);
+      result.height = Math.floor(scaleY1);
+      result.fScaleToTargetWidth = true;
+    } else {
+      result.width = Math.floor(scaleX2);
+      result.height = Math.floor(scaleY2);
+      result.fScaleToTargetWidth = false;
+    }
+    result.targetleft = Math.floor((targetwidth - result.width) / 2);
+    result.targettop = Math.floor((targetheight - result.height) / 2);
+
+    return result;
   },
 
+  suspend() {
+    this.suspended = true;
+    if (this.timer !== null) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+  },
+  resume() {
+    this.suspended = false;
+    if (this.timer === null) {
+      this.startTimer();
+    }
+  },
 
-	getDomFS: function() {
-		let self = this;
-		// if wrapper div not yet created
-		if(this.wrapper ==null)
-		// create it once, try to reduce image flash on change
-		{this.wrapper = document.createElement("div");
-		  this.bk=document.createElement("div");
-			this.bk.className="bgimagefs";
-			if(this.config.fill== true){
-				this.bk.style["filter"] ="blur("+this.config.blur+"px)";
-				this.bk.style["-webkit-filter"] ="blur("+this.config.blur+"px)";
-			}
-			else
-			{this.bk.style.backgroundColor = this.config.backgroundColor;}
-			this.wrapper.appendChild(this.bk);
-			this.fg=document.createElement("div");
-			this.wrapper.appendChild(this.fg);
-		}
-		if(this.photos.length) {
+  getDom() {
+    if (this.fullscreen) {
+      return this.getDomFS();
+    }
+    return this.getDomnotFS();
+  },
 
-			// get the size of the margin, if any, we want to be full screen
-			let m = window.getComputedStyle(document.body,null).getPropertyValue("margin-top");
-			// set the style for the containing div
+  getDomnotFS() {
+    const self = this;
+    const wrapper = document.createElement("div");
+    const photoImage = this.randomPhoto();
 
-			this.fg.style.border = "none";
-			this.fg.style.margin = "0px";
+    if (photoImage) {
+      const img = document.createElement("img");
+      img.src = photoImage.url;
+      img.id = "mmm-images-photos";
+      img.style.maxWidth = this.config.maxWidth;
+      img.style.maxHeight = this.config.maxHeight;
+      img.style.opacity = self.config.opacity;
+      img.className = "bgimage";
+      wrapper.appendChild(img);
+      self.startTimer();
+    }
+    return wrapper;
+  },
 
-			var photoImage = this.randomPhoto();
-			var img = null;
-			if (photoImage) {
+  getDomFS() {
+    const self = this;
+    // If wrapper div not yet created
+    if (this.wrapper === null) {
+      // Create it once, try to reduce image flash on change
 
-				// create img tag element
-				img = document.createElement("img");
+      this.wrapper = document.createElement("div");
+      this.bk = document.createElement("div");
+      this.bk.className = "bgimagefs";
+      if (this.config.fill === true) {
+        this.bk.style.filter = `blur(${this.config.blur}px)`;
+        this.bk.style["-webkit-filter"] = `blur(${this.config.blur}px)`;
+      } else {
+        this.bk.style.backgroundColor = this.config.backgroundColor;
+      }
+      this.wrapper.appendChild(this.bk);
+      this.fg = document.createElement("div");
+      this.wrapper.appendChild(this.fg);
+    }
+    if (this.photos.length) {
+      // Get the size of the margin, if any, we want to be full screen
+      const m = window
+        .getComputedStyle(document.body, null)
+        .getPropertyValue("margin-top");
+      // Set the style for the containing div
 
-				// set default position, corrected in onload handler
-				img.style.left = 0+"px";
-				img.style.top = document.body.clientHeight+(parseInt(m)*2);
-				img.style.position="relative";
+      this.fg.style.border = "none";
+      this.fg.style.margin = "0px";
 
-				img.src = photoImage.url;
-				// make invisible
-				img.style.opacity = 0;
-				// append this image to the div
-				this.fg.appendChild(img);
+      const photoImage = this.randomPhoto();
+      let img = null;
+      if (photoImage) {
+        // Create img tag element
+        img = document.createElement("img");
 
-				// set the onload event handler
-				// the loadurl request will happen when the html is returned to MM and inserted into the dom.
-				img.onload=  (evt) => {
+        // Set default position, corrected in onload handler
+        img.style.left = `${0}px`;
+        img.style.top = document.body.clientHeight + parseInt(m, 10) * 2;
+        img.style.position = "relative";
 
-					// get the image of the event
-					let img = evt.currentTarget;
-					Log.log("image loaded="+img.src+" size="+img.width+":"+img.height);
+        img.src = photoImage.url;
+        // Make invisible
+        img.style.opacity = 0;
+        // Append this image to the div
+        this.fg.appendChild(img);
 
-					// what's the size of this image and it's parent
-					var w = img.width;
-					var h = img.height;
-					var tw = document.body.clientWidth+(parseInt(m)*2);
-					var th = document.body.clientHeight+(parseInt(m)*2);
+        /*
+         * Set the onload event handler
+         * The loadurl request will happen when the html is returned to MM and inserted into the dom.
+         */
+        img.onload = (evt) => {
+          // Get the image of the event
+          const eventImage = evt.currentTarget;
+          Log.log(
+            `image loaded=${eventImage.src} size=${eventImage.width}:${eventImage.height}`
+          );
 
-					// compute the new size and offsets
-					var result = self.ScaleImage(w, h, tw, th, true);
+          // What's the size of this image and it's parent
+          const w = eventImage.width;
+          const h = eventImage.height;
+          const tw = document.body.clientWidth + parseInt(m, 10) * 2;
+          const th = document.body.clientHeight + parseInt(m, 10) * 2;
 
-					// adjust the image size
-					img.width = result.width;
-					img.height = result.height;
+          // Compute the new size and offsets
+          const result = self.scaleImage(w, h, tw, th, true);
 
-					Log.log("image setting size to "+result.width+":"+result.height);
-					Log.log("image setting top to "+result.targetleft+":"+result.targettop);
+          // Adjust the image size
+          eventImage.width = result.width;
+          eventImage.height = result.height;
 
-					// adjust the image position
-					img.style.left = result.targetleft+"px";
-					img.style.top = result.targettop+"px";
+          Log.log(`image setting size to ${result.width}:${result.height}`);
+          Log.log(
+            `image setting top to ${result.targetleft}:${result.targettop}`
+          );
 
-					// if another image was already displayed
-					let c = self.fg.childElementCount;
-					if( c>1)
-					{
-						for( let i =0 ; i<c-1;i++){
-							// hide it
-							self.fg.firstChild.style.opacity=0;
-							self.fg.firstChild.style.backgroundColor = "rgba(0,0,0,0)";
-							// remove the image element from the div
-							self.fg.removeChild(self.fg.firstChild);
-						}
-					}
-					self.fg.firstChild.style.opacity = self.config.opacity;
+          // Adjust the image position
+          eventImage.style.left = `${result.targetleft}px`;
+          eventImage.style.top = `${result.targettop}px`;
 
-					self.fg.firstChild.style.transition = "opacity 1.25s";
-					if(self.config.fill== true){
-						self.bk.style.backgroundImage = "url("+self.fg.firstChild.src+")";
-					}
-					self.startTimer();
-				};
-			}
-		}
-		return this.wrapper;
-	},
+          // If another image was already displayed
+          const c = self.fg.childElementCount;
+          if (c > 1) {
+            for (let i = 0; i < c - 1; i++) {
+              // Hide it
+              self.fg.firstChild.style.opacity = 0;
+              self.fg.firstChild.style.backgroundColor = "rgba(0,0,0,0)";
+              // Remove the image element from the div
+              self.fg.removeChild(self.fg.firstChild);
+            }
+          }
+          self.fg.firstChild.style.opacity = self.config.opacity;
 
-	getScripts: function() {
-		return ["MMM-ImagesPhotos.css"];
-	},
+          self.fg.firstChild.style.transition = "opacity 1.25s";
+          if (self.config.fill === true) {
+            self.bk.style.backgroundImage = `url(${self.fg.firstChild.src})`;
+          }
+          self.startTimer();
+        };
+      }
+    }
+    return this.wrapper;
+  },
 
-	processPhotos: function(data) {
-		var self = this;
-		this.photos = data;
-		if (this.loaded === false) {
-			if(this.suspended==false) {
-				self.updateDom(self.config.animationSpeed) ;
-			}
-		}
-		this.loaded = true;
-	},
+  getScripts() {
+    return ["MMM-ImagesPhotos.css"];
+  },
 
+  processPhotos(data) {
+    const self = this;
+    this.photos = data;
+    if (this.loaded === false) {
+      if (this.suspended === false) {
+        self.updateDom(self.config.animationSpeed);
+      }
+    }
+    this.loaded = true;
+  }
 });
--- README.md
+++ README.md
@@ -1,28 +1,30 @@
 # MMM-ImagesPhotos
-This is a module for the [MagicMirror](https://github.com/MichMich/MagicMirror). It will show photos from a directory.
 
-This module reads the images from the *uploads* directory inside the module.
-**Directory**: *MagicMirror/modules/MMM-ImagesPhotos/uploads*
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror). It will show photos from a directory.
 
+This module reads the images from the _uploads_ directory inside the module.
+**Directory**: `~/MagicMirror/modules/MMM-ImagesPhotos/uploads`
 
 ## Installation
+
 1. Clone this repository inside your MagicMirror's `modules` folder
 
-   `cd ~/MagicMirror/modules`
-
-   `git clone https://github.com/sdetweil/MMM-ImagesPhotos.git`
-
-   `cd MMM-ImagesPhotos`
-
-   `npm install`
+```bash
+cd ~/MagicMirror/modules
+git clone https://github.com/sdetweil/MMM-ImagesPhotos
+cd MMM-ImagesPhotos
+npm install
+```
 
 ## How it looks
+
 ![Demo](.github/animate.gif)
 
 ## Config
+
 The entry in `config.js` can include the following options:
 
-
+<!-- prettier-ignore-start -->
 | Option             | Description
 |--------------------|-----------
 | `opacity`          | The opacity of the image.<br><br>**Type:** `double`<br>Default 0.9
@@ -33,22 +35,24 @@ The entry in `config.js` can include the following options:
 | `maxHeight`        | Value for maximum height. Optional, possible values: absolute (e.g. "400px") or relative ("70%") <br> Default 100%
 | `backgroundColor`  | Value for color used to fill around the image if not fullscreen, default 'black', can be #xxyyzz, like #808080 (grey),<br> if fill is true, the backgroundColor setting is ignored
 | `fill`             | true or false, default false. instead of color use a blurred copy of the image to fill around the image
-| `blur`             | the size of the pixel blur of the background fill, default 8
+| `blur`             | The size of the pixel blur of the background fill, default 8
 | `sequential`       | true or false, default false, whether to process the images randomly(default) or sequentially
+<!-- prettier-ignore-end -->
 
 Here is an example of an entry in `config.js`
-```
+
+```js
 {
-	module: "MMM-ImagesPhotos",
-	position: "middle_center",
-	config: {
-		opacity: 0.9,
-		animationSpeed: 500,
-		updateInterval: 5000,
-		backgroundColor: 'grey',  // not used if fill is true
-		fill: false,   // fill around image with blurred copy of image
-		blur: 10,   // only used if fill is true
-		sequential: false  // process the image list randomly
-	}
+ module: "MMM-ImagesPhotos",
+ position: "middle_center",
+ config: {
+  opacity: 0.9,
+  animationSpeed: 500,
+  updateInterval: 5000,
+  backgroundColor: 'grey',  // not used if fill is true
+  fill: false,   // fill around image with blurred copy of image
+  blur: 10,   // only used if fill is true
+  sequential: false  // process the image list randomly
+ }
 },
 ```
